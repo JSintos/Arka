d71736097581a4b2e7898486785af880c4e2ec00
Joshua Sintos <jsintos15@gmail.com>
2022-11-14 00:43:22 +0800

    Added a profanity filter to the chat feature

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,6 +4,7 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
+const Filter = require("bad-words");
 const { default: Echo } = require("laravel-echo");
 
 require("./bootstrap");
@@ -78,7 +79,9 @@ const app = new Vue({
             this.messages.push(message);
             //POST request to the messages route with the message data in order for our Laravel server to broadcast it.
             axios.post("/messages", message).then((response) => {
-                // console.log(response.data);
+                if (response.data.status == "Message Sent!") {
+                    this.fetchMessages();
+                }
             });
         },
         firstBadge(data) {
