b31dffd15854c9dd517d03bbe76880ddecbc2d39
yumiterayama <77718869+yumiterayama@users.noreply.github.com>
2023-02-28 09:54:23 +0800

    feedback feedback feature with digestible rating

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,6 +4,42 @@
 @section('title','ARKA')
 
 <meta name="valid" content="{{ $valid }}">
+<!-- Stars Modal --> 
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+        <form method="POST" action="{{ route('storeMonthlyFeedback') }}">
+                @csrf
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Academic performance check</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+      <h3 style="text-align: center;">Rate your academic performance after using Arka</h3>
+                    <p>
+                    <div class="rating" id="monthlyFeedback" name="monthlyFeedback">
+                        <div class="star-icon">
+                            <input type="radio" value="1" name="rating" checked id="rating1" >
+                            <label for="rating1" class="fa fa-star"></label>
+                            <input type="radio" value="2" name="rating" id="rating2">
+                            <label for="rating2" class="fa fa-star"></label>
+                            <input type="radio" value="3" name="rating" id="rating3">
+                            <label for="rating3" class="fa fa-star"></label>
+                            <input type="radio" value="4" name="rating" id="rating4">
+                            <label for="rating4" class="fa fa-star"></label>
+                            <input type="radio" value="5" name="rating" id="rating5">
+                            <label for="rating5" class="fa fa-star"></label>
+                        </div>
+                    </div>
+      </div>
+      <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+            </form>
+    </div>
+  </div>
+</div>
 
 <div class="admin">
     <div class="container mt-5 mb-5">
@@ -40,10 +76,17 @@
             </div>
             @endforeach
         </div>
+        <!-- Button trigger modal -->
+        <div class="mt-5">
+            <button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#exampleModal">
+            Rate us now
+            </button>
+        </div>
     </div>
 </div>
 
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<!-- Old star-rating feature -->
+<!-- <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
             <form method="POST" action="{{ route('storeMonthlyFeedback') }}">
@@ -55,7 +98,7 @@
                     </button>
                 </div>
                 <div class="modal-body">
-                <h3 style="text-align: center;">Rate your academic performance for the previous month</h3>
+                <h3 style="text-align: center;">Rate your academic performance after using Arka</h3>
                     <p>
                     <div class="rating" id="monthlyFeedback" name="monthlyFeedback">
                         <div class="star-icon">
@@ -80,7 +123,7 @@
             </form>
         </div>
     </div>
-</div>
+</div> -->
 
 <script type="application/javascript">
     var valid = $('meta[name="valid"]').attr('content');
